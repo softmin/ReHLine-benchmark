6ca807c1edd93ad2b9d2bb6a697d1d60265e230d
Yixuan Qiu <yixuanq@gmail.com>
2023-10-20 09:56:05 +0800

    full running script

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-# cd benchmark_SVM
-# benchopt run . -d classification_data --n-repetitions 10 --timeout 1000
+cd benchmark_SVM
+benchopt run . -d classification_data --n-repetitions 10 --timeout 1000
 
 cd ../benchmark_sSVM
 benchopt run . -d classification_data --n-repetitions 10 --timeout 1000
